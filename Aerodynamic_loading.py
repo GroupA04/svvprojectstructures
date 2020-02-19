ab4a622059db36acd8f99dfbc616baebb821ddb2
jillz-a <jillz-a@users.noreply.github.com>
2020-02-19 08:48:08 +0100

    Added 3d plot function

---
+++ Aerodynamic_loading.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#variables
+
+N_z = 81 #sections z axis (rows)
+N_x = 41 #sections x axis (columns)
+C_a = 0.505 #chord length aileron in [m]
+l_a = 1.611 #span aileron in [m]
+
+#start code ----------------------------------------------------------------------------------------------------------------
+#import data
+aerodata = np.genfromtxt('aerodynamicloadf100.dat', delimiter=',')  #aerodynamic loading data given
+
+zcoordinates = np.zeros([N_z])
+xcoordinates = np.zeros([N_x])
+
+for i in range(1, N_z + 1 + 1):
+    theta_zi = (i-1)/N_z *np.pi
+    theta_zi1 = (i+1-1)/N_z *np.pi
+    z_i = -1/2 * (C_a / 2*(1-np.cos(theta_zi)) + C_a/2 *(1-np.cos(theta_zi1)))
+    if i <N_z +1:
+        zcoordinates[i-1] = z_i
+
+
+for i in range(N_x + 1 + 1):
+    theta_xi = (i-1)/N_x *np.pi
+    theta_xi1 = (i - 1 + 1) / N_x * np.pi
+    x_i = 1/2 * (l_a / 2*(1-np.cos(theta_xi)) + l_a/2 *(1-np.cos(theta_xi1)))
+    if i <N_x +1:
+        xcoordinates[i-1] = x_i
+
+#plotting for graphical representation
+
+xcoordinates,zcoordinates = np.meshgrid(xcoordinates,zcoordinates)
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.plot_surface(xcoordinates, zcoordinates, aerodata)
+ax.set_xlabel('Spanwise axis [m]')
+ax.set_ylabel('Chordwise axis [m]')
+ax.set_zlabel('Aerodynamic loading [kN/m2]')
+plt.show() 
---
+++ DataSheetValuesF100.py
@@ -0,0 +1,20 @@
+#All variables from Fokker 100 Data sheet
+#Everything defined as meter, Newton, or deg
+
+C_a = 0.505 # [m] 
+l_a = 1.611 # [m]
+x_1 = 0.125 # [m]
+x_2 = 0.498 # [m]
+x_3 = 1.494 # [m]
+x_a = 0.245 # [m]
+h = 0.161   # [m] 
+t_sk = 0.0011 # [m] 
+t_sp = 0.0024 # [m] 
+t_st = 0.0012 # [m] 
+h_st = 0.012  # [m] 
+w_st = 0.017  # [m] 
+n_st = 11
+d_1 = 0.00389 # [m] 
+d_3 = 0.01245 # [m] 
+theta = 30 # [degrees]
+P = 49200  # [Newton]
