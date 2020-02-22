bb5b78b407143fd5b0feada8e42bd956170a043e
jillz-a <jillz-a@users.noreply.github.com>
2020-02-22 13:33:57 +0100

    added validation file plotted validation node points

---
+++ validation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#get node locations from data file
+
+nodes = np.genfromtxt('nodes.txt', delimiter = ',')
+
+x_loc = nodes[:,1]
+y_loc = nodes[:,2]
+z_loc = nodes[:,3]
+
+# X,Y = np.meshgrid(x_loc,y_loc)
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.scatter(x_loc, y_loc, z_loc)
+ax.set_xlabel('Spanwise axis')
+ax.set_ylabel('height axis')
+ax.set_zlabel('chordwise axis')
+plt.show()
