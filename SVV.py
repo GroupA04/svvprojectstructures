c8039a3ba02c6303b27827507e4ed5d1051cc1e1
jillz-a <jillz-a@users.noreply.github.com>
2020-02-23 16:06:18 +0100

    svv file addition of variables and tweeks

--- SVV.py
+++ SVV.py
@@ -3,15 +3,18 @@ import numpy as np
 from Aerodynamic_loading import xcoordinates, zcoordinates, q_disc
 from numerical_functions import interpolate, integration
 
-N_x = 81                                         # Number of points in span-wise direction (x-direction) --> should be 100?
+N_x = 41                                         # Number of points in span-wise direction (x-direction) --> should be 100?
+
 q_x, z_cp = q_disc(zcoordinates)                       # This contains the values of q at each point along the span
-s, s_acum, s_out = integration(xcoordinates, 0, 1.611, q_x, 100)
-individ_integrals = [s]                          # This will have the areas of all the little individual "bars" (will have one less value than N_x)
-integral1 = [s_acum]                                  # This will have the accumulated area at each point in x
-xcoordinates = xcoordinates              # This will contain all the xcoordinates of our "processed" / "non-raw" data
+# s_acum, s_out = integration(xcoordinates, 0, 1.611, q_x, 100)
 
 
-def cumintegration(N_x , q):
+
+
+def cumintegration(N_x , q, x):
+    integral1 = []  # This will have the accumulated area at each point in x
+    xcoordinates = x  # This will contain all the xcoordinates of our "processed" / "non-raw" data
+    individ_integrals = []  # This will have the areas of all the little individual "bars" (will have one less value than N_x)
     for i in range(N_x - 1):                        # Minus 1 because we calculate area between i and i+1, so final point is not used
         print(i)
         s_i = (xcoordinates[i+1] - xcoordinates[i]) * ((q[i+1] - q[i])/2 + q[i])      # Calculates individual area
@@ -38,6 +41,7 @@ def cumintegration(N_x , q):
     integral1 = np.append([0],integral1)
     
     print("integral1 = ", integral1)
-
+    return integral1
 # We will have to explain in the report that each time that we integrate we "lose" a point and this introduces a small error
 # Taking more points in the x direction will reduce this error since the percentage of points "lost" goes down
+cumintegration(N_x, q_x, xcoordinates)
